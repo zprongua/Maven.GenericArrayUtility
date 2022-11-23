4cc3aa79dd2276493179b29f2c9424eaf3b9994e
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-22 21:49:16 -0500

    all but removeValue

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,65 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <Object> {
+    Object[] arrayUtilityArray;
+
+    public ArrayUtility(Object[] inputArray) {
+        arrayUtilityArray = inputArray;
+    }
+
+    public Object[] removeValue(Object valueToRemove) {
+        for (int i=0; i<arrayUtilityArray.length-1; i++) {
+            if (arrayUtilityArray[i].equals(valueToRemove)) {
+                arrayUtilityArray[i] = arrayUtilityArray[i+1];
+            }
+        }
+        return arrayUtilityArray;
+    }
+
+    public Integer getNumberOfOccurrences(Object valueToEvaluate) {
+        Integer numberOfOccurrences = 0;
+        for (int i=0; i<arrayUtilityArray.length; i++) {
+            if (arrayUtilityArray[i].equals(valueToEvaluate)) {
+                numberOfOccurrences++;
+            }
+        }
+        return numberOfOccurrences;
+    }
+
+    public Object getMostCommonFromMerge(Object[] arrayToMerge) {
+        Object mostCommon = null;
+        Integer most = 0;
+        Integer count = 0;
+        ArrayList<Object> ao = new ArrayList<>(Arrays.asList(arrayUtilityArray));
+        ao.addAll(Arrays.asList(arrayToMerge));
+        for (Object o : ao) {
+            for (Object p : ao) {
+                if (o.equals(p)) count++;
+            }
+            if (count>most) {
+                most = count;
+                mostCommon = o;
+            }
+            count=0;
+        }
+        return mostCommon;
+    }
+
+    public Integer countDuplicatesInMerge(Object[] arrayToMerge, Object valueToEvaluate) {
+        Integer count = 0;
+        ArrayList<Object> ao = new ArrayList<>(Arrays.asList(arrayUtilityArray));
+        ao.addAll(Arrays.asList(arrayToMerge));
+        for (Object o : ao) {
+            if (o.equals(valueToEvaluate)) count++;
+        }
+        return count;
+    }
 }
--- src/test/java/com/zipcodewilmington/arrayutility/RemoveValueTest.java
+++ src/test/java/com/zipcodewilmington/arrayutility/RemoveValueTest.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
-import com.zipcodewilmington.UnitTestingUtils;
+import com.zipcodewilmington.arrayutility.UnitTestingUtils;
 import org.junit.Test;
 
 /**
