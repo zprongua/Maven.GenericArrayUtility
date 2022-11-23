e3c535f4936099f0d539f6207a8f81a85bc1d6c5
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-22 23:28:39 -0500

    removeValue complete, all tests pass

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,25 +3,26 @@ package com.zipcodewilmington.arrayutility;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility <Object> {
-    Object[] arrayUtilityArray;
+public class ArrayUtility <T> {
+    T[] arrayUtilityArray;
 
-    public ArrayUtility(Object[] inputArray) {
+    public ArrayUtility(T[] inputArray) {
         arrayUtilityArray = inputArray;
     }
 
-    public Object[] removeValue(Object valueToRemove) {
-        for (int i=0; i<arrayUtilityArray.length-1; i++) {
-            if (arrayUtilityArray[i].equals(valueToRemove)) {
-                arrayUtilityArray[i] = arrayUtilityArray[i+1];
+    public T[] removeValue(T valueToRemove) {
+        ArrayList<T> al = new ArrayList<>();
+        for (Object o : arrayUtilityArray) {
+            if (!o.equals(valueToRemove)) {
+                al.add((T) o);
             }
         }
-        return arrayUtilityArray;
+        return toArray(al);
     }
 
     public Integer getNumberOfOccurrences(Object valueToEvaluate) {
@@ -34,8 +35,8 @@ public class ArrayUtility <Object> {
         return numberOfOccurrences;
     }
 
-    public Object getMostCommonFromMerge(Object[] arrayToMerge) {
-        Object mostCommon = null;
+    public T getMostCommonFromMerge(Object[] arrayToMerge) {
+        T mostCommon = null;
         Integer most = 0;
         Integer count = 0;
         ArrayList<Object> ao = new ArrayList<>(Arrays.asList(arrayUtilityArray));
@@ -46,7 +47,7 @@ public class ArrayUtility <Object> {
             }
             if (count>most) {
                 most = count;
-                mostCommon = o;
+                mostCommon = (T) o;
             }
             count=0;
         }
@@ -61,5 +62,13 @@ public class ArrayUtility <Object> {
             if (o.equals(valueToEvaluate)) count++;
         }
         return count;
+    }
+
+    public T[] toArray(List<T> list) {
+        T[] newArray = (T[]) Array.newInstance(list.get(0).getClass(), list.size());
+        for (int i=0; i < list.size(); i++) {
+            newArray[i] = list.get(i);
+        }
+        return newArray;
     }
 }
